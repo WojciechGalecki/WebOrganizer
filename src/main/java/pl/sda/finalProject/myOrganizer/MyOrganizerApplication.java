ae169ef1a4deade896a94ffe968b97eada84bbb1
Wojtek <wojciech.galecki@wp.pl>
2018-02-20 17:36:33 +0100

    initial commit

---
+++ src/main/java/pl/sda/finalProject/myOrganizer/MyOrganizerApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.finalProject.myOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOrganizerApplication.class, args);
+	}
+}
