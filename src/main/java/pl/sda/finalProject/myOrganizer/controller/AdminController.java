40a9a61634f06de38cc2e569d2ca773768eba3bc
Wojtek <wojciech.galecki@wp.pl>
2018-02-24 19:00:47 +0100

    added pagination for user list -> not working properly

--- src/main/java/pl/sda/finalProject/myOrganizer/controller/AdminController.java
+++ src/main/java/pl/sda/finalProject/myOrganizer/controller/AdminController.java
@@ -1,6 +1,7 @@
 package pl.sda.finalProject.myOrganizer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +19,11 @@ public class AdminController {
     @Autowired
     private IUserRepository userRepository;
 
-    @GetMapping("/users")
-    public String showUsers(Model model) {
+    @GetMapping(path = "/users")
+    public String showUsers(Model model, @RequestParam(value = "page", defaultValue = "1") int page) {
         model.addAttribute("users", userService.findAllUsers());
+        model.addAttribute("pages",userRepository.findAll(new PageRequest(page, 4)));
+        model.addAttribute("currentPage", page);
         return "users";
     }
 
