153a0b7c7c7671a0235d2fb4ef4ecd636cbb8555
Wojtek <wojciech.galecki@wp.pl>
2018-02-24 16:52:08 +0100

    added option delete user by admin

--- src/main/java/pl/sda/finalProject/myOrganizer/controller/AdminController.java
+++ src/main/java/pl/sda/finalProject/myOrganizer/controller/AdminController.java
@@ -3,10 +3,8 @@ package pl.sda.finalProject.myOrganizer.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.finalProject.myOrganizer.dao.IUserRepository;
 import pl.sda.finalProject.myOrganizer.entity.MyUser;
 import pl.sda.finalProject.myOrganizer.model.UserModel;
 import pl.sda.finalProject.myOrganizer.service.UserService;
@@ -17,6 +15,8 @@ public class AdminController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private IUserRepository userRepository;
 
     @GetMapping("/users")
     public String showUsers(Model model) {
@@ -24,13 +24,14 @@ public class AdminController {
         return "users";
     }
 
-    @GetMapping("/users/delete/{email}")
-    public String deleteUser(@PathVariable("email") String email){
-        if(userService.findUserByEmail(email) == null){
+    @PostMapping("/users/delete")
+    public String deleteUser(@RequestParam("email") String email) {
+        if (userService.findUserByEmail(email) == null) {
             return "userNotFound";
         }
         userService.deleteUser(email);
-        return "redirect:/users";
+        return "redirect:/organizer/users";
     }
+
 
 }
--- src/test/java/pl/sda/finalProject/myOrganizer/MyOrganizerApplicationTests.java
+++ src/test/java/pl/sda/finalProject/myOrganizer/MyOrganizerApplicationTests.java
@@ -1,4 +1,5 @@
 package pl.sda.finalProject.myOrganizer;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,37 +12,50 @@ import pl.sda.finalProject.myOrganizer.service.UserService;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 public class MyOrganizerApplicationTests {
 
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private IUserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private IUserRepository userRepository;
 
-	@Before
-	public void initDB(){
+    @Before
+    public void initDB() {
 
-		MyUser user = MyUser.builder().email("user@user.pl").password("12345")
-				.userName("user").build();
+        MyUser user = MyUser.builder().email("user@user.pl").password("12345")
+                .userName("user").build();
 
-		MyUser admin = MyUser.builder().email("admin@user.pl").password("12345")
-				.userName("admin").build();
+        MyUser admin = MyUser.builder().email("admin@user.pl").password("12345")
+                .userName("admin").build();
 
-		userRepository.save(user);
-		userRepository.save(admin);
+        userRepository.save(user);
+        userRepository.save(admin);
 
-	}
+    }
 
-	@Test
-	public void findUserByEmail(){
-		MyUser user1 = userRepository.findOne("user@user.pl");
-		assertNotNull(user1);
-		MyUser user2 = userService.findUserByEmail("admin@user.pl");
-		assertEquals(user2.getEmail(),"admin@user.pl");
-	}
+    @Test
+    public void findUserByEmail() {
+        MyUser user1 = userRepository.findOne("user@user.pl");
+        assertNotNull(user1);
+        MyUser user2 = userService.findUserByEmail("admin@user.pl");
+        assertEquals(user2.getEmail(), "admin@user.pl");
+    }
 
+    @Test
+    public void deleteUserByEmail() {
+        userService.deleteUser("user@user.pl");
+        assertTrue(userService.findUserByEmail("user@user.pl") == null);
+    }
+
+    @Test
+    public void getEmail(){
+        String email1 = userService.findUserByEmail("user@user.pl").getEmail();
+        assertEquals(email1,"user@user.pl");
+    }
 }
