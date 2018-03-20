7c41c7eba716c76fa24b2b9cb94bc3c72d8c5bf7
Wojtek <wojciech.galecki@wp.pl>
2018-03-20 18:29:19 +0100

    configuration myTests class - simple tests

--- src/test/java/pl/sda/finalProject/myOrganizer/myTests/myTests.java
+++ src/test/java/pl/sda/finalProject/myOrganizer/myTests/myTests.java
@@ -2,7 +2,11 @@ package pl.sda.finalProject.myOrganizer.myTests;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import pl.sda.finalProject.myOrganizer.dao.ITaskRepository;
 import pl.sda.finalProject.myOrganizer.dao.IUserRepository;
 import pl.sda.finalProject.myOrganizer.entity.MyUser;
@@ -12,6 +16,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class myTests {
 
     @Autowired
